971e5bcaa483ada1b9d04154adbcc7ba0f4e8f15
Paulo Salvatore <salvatoregames@gmail.com>
2022-09-11 01:18:17 -0300

    Initial files

---
+++ src/app/configs/settingsConfig.js
@@ -0,0 +1,36 @@
+import themesConfig from "app/configs/themesConfig";
+import i18n from "../../i18n";
+
+const settingsConfig = {
+  layout: {
+    style: "layout2", // layout1 layout2 layout3
+    config: {
+      navbar: {
+        display: false,
+      },
+      footer: {
+        display: false,
+      },
+    }, // checkout default layout configs at app/theme-layouts for example  app/theme-layouts/layout1/Layout1Config.js
+  },
+  customScrollbars: true,
+  direction: i18n.dir(i18n.options.lng) || "ltr", // rtl, ltr
+  theme: {
+    main: themesConfig.light9,
+    navbar: themesConfig.light9,
+    toolbar: themesConfig.light9,
+    footer: themesConfig.light9,
+  },
+  /*
+   To make whole app auth protected by default set defaultAuth:['admin','staff','user']
+   To make whole app accessible without authorization by default set defaultAuth: null
+   *** The individual route configs which has auth option won't be overridden.
+   */
+  defaultAuth: null,
+  /*
+    Default redirect url for the logged-in user,
+   */
+  loginRedirectUrl: "/",
+};
+
+export default settingsConfig;
